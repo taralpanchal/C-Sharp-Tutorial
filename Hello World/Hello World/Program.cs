f075ac979ec6d824a351a1739e7a509b60f7d709
Taral Panchal <taral.facile@outlook.com>
2021-03-11 14:29:38 +0530

    declare variables in C#

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var price = 10.25f;
+            var character = 'A';
+            var firstName = "Mosh";
+            var isWorking = true;
+
+            Console.WriteLine("Hello World");
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(price);
+        }
+    }
+ }
+ 
