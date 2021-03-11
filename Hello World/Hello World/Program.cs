302a251a465bbd173453879e96a814e556fa1fbe
Taral Panchal <taral.facile@outlook.com>
2021-03-11 14:56:07 +0530

    Type conversion

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -6,21 +6,23 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            var number = 2;
-            var count = 10;
-            var price = 10.25f;
-            var character = 'A';
-            var firstName = "Mosh";
-            var isWorking = true;
+            try
+            {
+                var number = 52;
+                string str = "Hello";
+                byte b = Convert.ToByte(number);
+                char c = Convert.ToChar(number);
+                bool bl = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                Console.WriteLine(bl);
 
-            Console.WriteLine("Hello World");
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
-            Console.WriteLine(price);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be convert to a byte");
+            }
         }
     }
  }
- 
+      
