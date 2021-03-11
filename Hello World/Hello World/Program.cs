b95f3c639a4a4fa626834303c8dbdba54bc46925
Taral Panchal <taral.facile@outlook.com>
2021-03-11 17:57:28 +0530

    Array

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -1,33 +1,25 @@
 ﻿using System;
 
 
-namespace Hello_World
-{
-    public class Person
-    {
-        public string FirstName;
-        public string LastName; 
-            
-        public void Introduse()
-        {
-
-            Console.WriteLine("Hi my name is " + FirstName + " " + LastName);
-        }
-    }
+namespace Hello_World { 
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.FirstName = "Taral";
-            person.LastName = "Panchal";
-            person.Introduse();
+            var numbers = new int[3];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
 
-            calculator cal = new calculator();
-            var result = cal.Add(10,10);
-            Console.WriteLine(result);
+            var name = new string[3] { "Taral", "Hardip", "Rajesh" };
+            Console.WriteLine(name[0]);
+            Console.WriteLine(name[1]);
+            Console.WriteLine(name[2]);
 
-           
+
         }
     }
  }
