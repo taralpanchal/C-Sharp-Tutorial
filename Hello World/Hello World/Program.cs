af4e18fda7920767dadf13cd08e8951fcf63782b
Taral Panchal <taral.facile@outlook.com>
2021-03-12 11:16:16 +0530

    value type & reference type

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -3,26 +3,23 @@
 
 namespace Hello_World
 {
-    public enum ShippingMethod
-    {
-        RegularAirMail = 1,
-        RegularedAirMail = 2,
-        Express = 3
-    }
     class Program
     {
         static void Main(string[] args)
+
         {
-            var method = ShippingMethod.Express;
-            Console.WriteLine((int)method);
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("a : {0} , b : {1}",a ,b));
 
-            var methodId = 2;
-            Console.WriteLine((ShippingMethod)methodId);
-            Console.WriteLine(method.ToString());
-
-            var methodName = "2";
-            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
-            Console.WriteLine(shippingMethod);
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(string.Format("array[0] : {0}, array[0] : {1}",array1[1],array2[1]));
+            Console.WriteLine(array1[0]);
+            Console.WriteLine(array1[1]);
+            Console.WriteLine(array1[2]);
         }
 
     }
