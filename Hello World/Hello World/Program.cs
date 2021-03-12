43e1bd818fe57df8a6ac8f63f56b94f2e7ac8be9
Taral Panchal <taral.facile@outlook.com>
2021-03-12 16:25:41 +0530

    Array Method

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -7,28 +7,40 @@ namespace Hello_World
 
         static void Main(string[] args)
         {
-            var random = new Random();
 
-            //for (var i = 0; i < 10; i++)
-            //{
-            //    Console.WriteLine(random.Next());
-            //}
+            var number = new[] { 2,5,6,8,9,10,1,5};
+            
+            //lenght
+            
+            Console.WriteLine("Length: " + number.Length);
+            
+            //Index of
+            Console.WriteLine(" Index Of: " + Array.IndexOf(number,10));
 
-            //for (var i = 0; i < 10; i++)
-            //    Console.WriteLine((char)random.Next(65,95));
-
-
-
-            const int passwordlenght = 10;
-
-            var buffer = new char[passwordlenght];
-
-            for (var i = 0; i < passwordlenght; i++)
+            //clear
+            Array.Clear(number, 0, 2);
+            Console.WriteLine("Effect of clear:");
+            foreach( var i in number)
             {
-                 buffer[i] = (char)('a' + random.Next(0,26));
+                Console.WriteLine(i);
             }
-                var password = new string(buffer);
-                Console.WriteLine(password);
+
+            //copy
+            int[] another = new int[5];
+            Array.Copy(number, another, 5);
+            Console.WriteLine("Copy array: ");
+            foreach (var i in another)
+            {
+                Console.WriteLine(i);
+            }
+
+            //sort
+
+            Array.Sort(number);
+            Console.WriteLine("Soring Effect:");
+            foreach(var i in number)
+                Console.WriteLine(i);
+            Array.Reverse(number);
 
         }
     }
