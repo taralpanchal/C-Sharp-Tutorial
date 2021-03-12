e282de4c9b0cdd7cb6807be14dc89bbd8cb0c957
Taral Panchal <taral.facile@outlook.com>
2021-03-12 14:47:30 +0530

    different loop

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -7,36 +7,38 @@ namespace Hello_World
 
         static void Main(string[] args)
         {
-            /* var hour = 15;
-             var goldCustomer = true;
-             float price;
-             if(goldCustomer)
-                 price = 25.50f;
-             else
-                 price = 30.70f;
+            //for loop
+            //for(var i=1; i<10; i++)
+            //    if(i %2 == 0)
+            //          Console.WriteLine();
 
-               if(hour > 0 && hour < 12)
-                   Console.WriteLine("Good Morning");
-               else if(hour >= 12 && hour < 18)
-                   Console.WriteLine("Good Afternoon");
-               else
-                   Console.WriteLine("Good Evening");
-             
-            // var price =  (goldCustomer) ? 18.80 : 23.50;
-            Console.WriteLine(price);*/
-            var session1 = session.Winter;
-            switch(session1)
+            //foreach loop
+            //var name = "Mosh Hamdani";
+            //foreach(var i in name)
+            //    Console.WriteLine(i);
+
+            //var number = new int[] { 1, 2, 3, 4, 5 };
+            //{
+            //    foreach(var num in number)
+            //        Console.WriteLine(num);
+            //}
+
+
+            //While loop
+          while(true)
             {
-                case  session.Autumn:
-                    Console.WriteLine("It's Autumn");
-                    break;
-                case session.Winter:
-                    Console.WriteLine("It's Winter");
-                    break;
-                default:
-                    Console.WriteLine("normal");
-                    break;
+                Console.Write("Enter Name:");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo :" + input);
+                    continue;
+                }
+                break;
+
             }
+           
         }
     }
 }
