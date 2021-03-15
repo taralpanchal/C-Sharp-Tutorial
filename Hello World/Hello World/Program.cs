8d413c25081903c16f9ad0c7c2e7ca539151442c
Taral Panchal <taral.facile@outlook.com>
2021-03-15 18:56:30 +0530

    Ex-5 count number 1 to 100 divisible by 3

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -11,30 +11,16 @@ namespace Hello_World
         
         static void Main(string[] args)
         {
-            //4 - Your job is to write a program for a speed camera.For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic.Write a program that asks the user to enter the speed limit.Once set, the program asks for the speed of a car.If the user enters a value less than the speed limit, program should display Ok on the console.If the value is above the speed limit, the program should calculate the number of demerit points.For every 5km / hr above the speed limit, 1 demerit points should be incurred and displayed on the console.If the number of demerit points is above 12, the program should display License Suspended.
-
-            Console.WriteLine("Enter Car Speed : ");
-            var speed = Convert.ToInt32(Console.ReadLine());
-
-            var speedlimit = 50;
-            
-            if(speedlimit > speed)
+            // 1 - Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.Display the count on the console.
+            var count = 0;
+            for (int i = 1; i <= 100; i++) 
             {
-                Console.WriteLine("Ok..!");
-            }
-            else{
-                const int kmperpoint = 5;
-                var countpoints = (speed - speedlimit) /kmperpoint; 
-                if(countpoints > 12)
+                if(i % 3 == 0)
                 {
-                    Console.WriteLine("License Suspended.");
-                }
-                else
-                {
-         
-                    Console.WriteLine("Your Point : "+countpoints);
+                    count++;
                 }
             }
+            Console.WriteLine(count);
         }
     }
 }
