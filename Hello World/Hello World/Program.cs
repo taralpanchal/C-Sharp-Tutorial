af8ba7812654d9e85eab5b7071351d4cf2b27177
Taral Panchal <taral.facile@outlook.com>
2021-03-15 18:50:18 +0530

    Ex-4

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -11,23 +11,30 @@ namespace Hello_World
         
         static void Main(string[] args)
         {
-            //Write a program and ask the user to enter the width and height of an image. Then tell if the image is landscape or portrait.
-                     
-            Console.Write("Enter Height: ");
-            var Height = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Width: ");
-            var Width = Convert.ToInt32(Console.ReadLine());
-            //if (Height > Width)
-            //{
-            //    Console.WriteLine("portrait");
+            //4 - Your job is to write a program for a speed camera.For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic.Write a program that asks the user to enter the speed limit.Once set, the program asks for the speed of a car.If the user enters a value less than the speed limit, program should display Ok on the console.If the value is above the speed limit, the program should calculate the number of demerit points.For every 5km / hr above the speed limit, 1 demerit points should be incurred and displayed on the console.If the number of demerit points is above 12, the program should display License Suspended.
 
-            //}
-            //else
-            //{
-            //    Console.WriteLine("landscape");
-            //}
-            var img = (Height > Width) ? "Portrait" : "landscape";
-            Console.WriteLine(img);
+            Console.WriteLine("Enter Car Speed : ");
+            var speed = Convert.ToInt32(Console.ReadLine());
+
+            var speedlimit = 50;
+            
+            if(speedlimit > speed)
+            {
+                Console.WriteLine("Ok..!");
+            }
+            else{
+                const int kmperpoint = 5;
+                var countpoints = (speed - speedlimit) /kmperpoint; 
+                if(countpoints > 12)
+                {
+                    Console.WriteLine("License Suspended.");
+                }
+                else
+                {
+         
+                    Console.WriteLine("Your Point : "+countpoints);
+                }
+            }
         }
     }
 }
